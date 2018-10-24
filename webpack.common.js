6b8dc3add81aa9c0d7bff789285ac19628e4a18d
Pawel Migut <pawel@devten.pl>
2018-10-24 21:29:37 +0200

    Split webpack config into prod and dev

--- webpack.common.js
+++ webpack.common.js
@@ -5,9 +5,6 @@ module.exports = {
     entry: {
         app: './app.js'
     },
-    devServer: {
-        contentBase: './dist'
-    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
