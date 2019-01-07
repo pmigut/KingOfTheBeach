681c846d66a3834c96da05b3e8bdc1460795eedb
Pawel Migut <pawel@devten.pl>
2019-01-07 12:27:24 +0100

    Use vue runtime+compiler package for build

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,10 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm.js'
+        }
+    }
 });
