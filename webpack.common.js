8e867e52b184579d0d0a2e730b26908dc6a7aa33
Pawel Migut <pawel@devten.pl>
2023-06-10 22:16:08 +0200

    Upgrade dependencies; Fix webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
     entry: {
@@ -11,7 +11,11 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-        new CopyWebpackPlugin(['index.html']),
+        new CopyWebpackPlugin({
+            patterns: [
+                'index.html',
+            ]
+        }),
         new VueLoaderPlugin()
     ],
     module: {
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,14 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        static: {
+            directory: './dist'
+        },
+        watchFiles: ['src/**/*', 'index.html']
     },
     resolve: {
         alias: {
